9c6c67186cfa4f6e2d769ac32188b199c7d9bd1e
Dominik R. Kocher <dominikraphael.kocher@students.bfh.ch>
2019-04-28 12:23:21 +0200

    tabs: modifications for adding goal page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -14,14 +14,15 @@ export class TabsPage {
   tab1 = JournalPage;
   tab2 = InformationDocumentationPage;
   tab3 = SupportPage;
-  tab4 = AdaptionsPage;
+  tab4 = SupportPage;
+  tab5 = AdaptionsPage;
 
   @ViewChild('mainTabs') tabRef: Tabs;
 
 
   constructor() {
   }
-  
+
   tabChanged($ev) {
     $ev.setRoot($ev.root);
   }
