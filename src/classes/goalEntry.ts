ac404c77d03319ae07d76f3cd31eb7dddfe56ef9
Dominik R. Kocher <dominikraphael.kocher@students.bfh.ch>
2019-05-10 16:11:28 +0200

    goals: GUI corrections

--- src/classes/goalEntry.ts
+++ src/classes/goalEntry.ts
@@ -8,7 +8,7 @@ export class GoalEntry {
   isAchieved: boolean;
 
   constructor(entryId: number, entryDate: Date, entryText: string, isRepetitive: boolean, isAchieved: boolean) {
-    this.entryId = entryId;
+    this.entryId = entryId; // test -> new Date().getTime() - 604800000;
     this.entryDate = entryDate;
     this.entryText = entryText;
     this.isRepetitive = isRepetitive;
